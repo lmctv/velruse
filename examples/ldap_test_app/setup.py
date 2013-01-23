0a76e90e4a39862fd7f84625369708f89239fd9c
Lorenzo M. Catucci <lorenzo@sancho.ccd.uniroma2.it>
2013-01-23 09:17:14 +0100

    Tidy-up deps.

--- examples/ldap_test_app/setup.py
+++ examples/ldap_test_app/setup.py
@@ -1,9 +1,7 @@
 from setuptools import setup, find_packages
 
 requires = [
-    'velruse',
-    'pyramid',
-    'pyramid_ldap>=0.2.dev',
+    'velruse[ldap]',
     'waitress',
     'redis',
     'pyramid_debugtoolbar',
